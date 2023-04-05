857119848bf8508cd430b3fd373f1331999acbb3
AnniePrakash <hannoshiningacademy@gmail.com>
2023-04-05 08:54:16 +0530

    My New GIT

---
+++ src/org/constmt/SampleControl.java
@@ -0,0 +1,18 @@
+package org.constmt;
+
+public class SampleControl {
+
+	public static void main(String[] args) {
+int a=1;
+
+do {
+	
+	System.out.println(a);
+	a++;
+}
+while(a<10);
+	
+
+	
+}
+}
